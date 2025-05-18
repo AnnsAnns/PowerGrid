e0f5f33d135f5d0a406926c904802166d52f5544
Ann🐸 <git@annsann.eu>
2025-05-18 15:52:05 +0200

    Turbine: Greatly increase performance through caching (#47)

--- turbine/src/parsing/mod.rs
+++ turbine/src/parsing/mod.rs
@@ -2,8 +2,10 @@ mod temperature;
 mod wind_data;
 mod download;
 mod read;
+mod cache;
 
 pub use temperature::TemperatureData;
 pub use wind_data::WindData;
 pub use download::{download_data_for, read_text_from_url};
 pub use read::read_for;
+pub use cache::Cache;
